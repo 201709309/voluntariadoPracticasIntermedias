2a62d43559c3ded216afd63f206d9e0d6b383d3d
201709309 <josesantizo149@gmail.com>
2021-08-21 09:49:00 -0600

    listos para empezar

---
+++ src/components/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './test.css';
+import CardItem from './CardItem';
+
+
+function Test() {
+  return(
+    <h1 className="cosa">
+      HOLA
+    </h1>
+  )
+}
+
+export default Test;
